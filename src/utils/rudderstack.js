7e9da13e038d712bd482febc5e93c9157b6f7d10
Shamil Ataev <shamilataev@Shamils-MacBook-Air.local>
2024-12-23 12:25:35 +0000

    feat: new source

--- src/utils/rudderstack.js
+++ src/utils/rudderstack.js
@@ -1,8 +1,8 @@
 import * as rudderanalytics from 'rudder-sdk-js';
 
 rudderanalytics.load(
-  '2qMbaaH28qs1fUr8MdbH02cwfyD',
-  'https://rudderstacudgv.dataplane.rudderstack.com'
+  '2qcKxjVX6xyzOXnDE6j6fCnFHsh',
+  'https://shamildemzlfpr.dataplane.rudderstack.com'
 );
 
 export default rudderanalytics;
