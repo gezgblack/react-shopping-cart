8c4aad0d199303375757c2064cd35f6c3491ac48
Shamil <shamil@Shamils-MacBook-Pro.local>
2025-11-29 10:08:28 +0000

    chore: update RudderStack write key

--- src/utils/rudderstack.js
+++ src/utils/rudderstack.js
@@ -1,7 +1,7 @@
 import * as rudderanalytics from 'rudder-sdk-js';
 
 rudderanalytics.load(
-  '2qcKxjVX6xyzOXnDE6j6fCnFHsh',
+  '369CwDwHPldEnGQltBxz2hNtzme',
   'https://shamildemzlfpr.dataplane.rudderstack.com'
 );
 
