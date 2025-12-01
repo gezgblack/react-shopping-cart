03590350a815801060fda75d7c45abea3a0cd13a
Shamil <shamil@Shamils-MacBook-Pro.local>
2025-12-01 14:55:44 +0000

    chore: update RudderStack dataplane URL

--- src/utils/rudderstack.js
+++ src/utils/rudderstack.js
@@ -2,7 +2,7 @@ import * as rudderanalytics from 'rudder-sdk-js';
 
 rudderanalytics.load(
   '2qcKxjVX6xyzOXnDE6j6fCnFHsh',
-  'https://rudderstacaihq.dataplane.rudderstack.com'
+  'https://shamildemzlfpr.dataplane.rudderstack.com'
 );
 
 // Expose rudderanalytics to the global window object for browser console access
